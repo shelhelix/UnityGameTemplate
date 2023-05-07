23f4f94c8edd3daa91f9466af8eeb0f613166723
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2023-05-07 12:44:41 +0300

    added stubs for end game screens

--- Assets/Scripts/Gameplay/GameplayStarter.cs
+++ Assets/Scripts/Gameplay/GameplayStarter.cs
@@ -1,9 +1,15 @@
-﻿using VContainer.Unity;
+﻿using Com.Shelinc.FullscreenCanvasController;
+using GameJamEntry.Gameplay.UI;
+using VContainer.Unity;
 
 namespace GameJamEntry.Gameplay {
 	public class GameplayStarter : IStartable {
+		ScreenManager _screenManager;
+		
+		public GameplayStarter(ScreenManager screenManager) => _screenManager = screenManager;
+
 		public void Start() {
-			// TODO: init your classes here
+			_screenManager.ShowScreen<GameplayScreen>().Forget();
 		}
 	}
 }
---
+++ Assets/Scripts/Gameplay/UI/ReturnToMenuButton.cs
@@ -0,0 +1,17 @@
+﻿using Com.Shelinc.SceneTransitionEffects;
+using Cysharp.Threading.Tasks;
+using GameComponentAttributes.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace GameJamEntry.Gameplay.UI {
+	public class ReturnToMenuButton : MonoBehaviour {
+		[NotNullReference] [SerializeField] Button Button;
+		
+		[Inject]
+		public void Init(SceneLoader sceneLoader) {
+			Button.onClick.AddListener(() => sceneLoader.LoadScene("MainMenu").Forget());
+		}
+	}
+}
