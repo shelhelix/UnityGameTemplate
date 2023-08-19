d9dbc7213bfa4c56b3f88bf079a0a65574e2e6f5
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2023-08-19 21:20:05 +0300

    added VContainer settings assets

---
+++ Assets/Scripts/GameStartScope.cs
@@ -0,0 +1,11 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace GameJamEntry {
+	public class GameStartScope : LifetimeScope {
+		protected override void Configure(IContainerBuilder builder) {
+			base.Configure(builder);
+			// TODO: add your game-wide necessary classes
+		}
+	}
+}
