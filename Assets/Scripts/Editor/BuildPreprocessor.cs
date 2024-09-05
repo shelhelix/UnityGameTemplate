5520a13ec0e7b6f4504292954f90a4f998d9b311
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2024-09-05 19:53:17 +0300

    Automatically set application version to the current commit hash in git

---
+++ Assets/Scripts/Editor/BuildPreprocessor.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace _GameAssets.Scripts.Editor {
+	public class BuildPreprocessor : IPreprocessBuildWithReport {
+		public int callbackOrder => 0;
+		
+		public void OnPreprocessBuild(BuildReport report) {
+			UpdateBuildVersion();
+		}
+
+		static void UpdateBuildVersion() {
+			// run git log --pretty=format:'%h' -n 1
+			var process = new Process {
+				StartInfo = new ProcessStartInfo {
+					FileName               = "git",
+					Arguments              = "log --pretty=format:'%h' -n 1",
+					UseShellExecute        = false,
+					RedirectStandardOutput = true,
+					CreateNoWindow         = true,
+				},
+			};
+			process.Start();
+			// read result
+			var commitHash = process.StandardOutput.ReadToEnd().Trim();
+			process.WaitForExit();
+			// set commit hash to build options
+			PlayerSettings.bundleVersion = commitHash.Replace("'", string.Empty);
+		}
+	}
+}
